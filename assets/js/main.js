18e5c390fc0f3e713ab9404830018f32113b192b
Collins <mutaic84@gmail.com>
2022-04-21 11:11:09 +0300

    fetch categories from api and display

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 let myApp = () => {
-    const categoriesList = document.getElementById("categories")
-    const apiUrl = 'https://opentdb.com/api.php?amount=1';
-    const categoryUrl = "https://opentdb.com/api_category.php";
-
-    function getCategories() {
-        fetch(categoryUrl)
-        .then(response => response.json())
-        .then(data => {
-            const categories = data.trivia_categories
-            categories.forEach(category => {
-                const itemContainer = document.createElement("li")
-                itemContainer.setAttribute("class", "animate js-category")
-                itemContainer.innerText = category.name
-                categoriesList.append(itemContainer)
-            })
-        })
-    }
-    getCategories()
+    let api = 'https://opentdb.com/api.php?amount=1';
+    let bool, categories, category, chosen, choices, correct, query;
+    let solo = true;
+    let turn = 1;
+    let counter = 0,
+    player1Score = 0,
+    player2Score = 0;
+    const apiCategory = 'https://opentdb.com/api_category.php';
+    const htmlEntities = str => String(str).replace(/&[#039]*;/g, "'").replace(/&[amp]*;/g, '&').replace(/&[quote]*;/g, '"');
+    const getCategories = () => {
+        $.getJSON(apiCategory, function (data) {
+        categories = data.trivia_categories;
+        $.each(categories, function (i) {
+            $('#category').append('<li class="animate js-category">' + data.trivia_categories[i].name + '</li>');
+        });
+        closeCategories();
+        });
+    };
 
 }
 
